3f0ae2ce80213d955ce8e8cc0a6d474ca8a93c7d
520zzj <44475605+520zzj@users.noreply.github.com>
2019-07-17 18:27:39 +0800

    增加基础样式

--- vue_cil/src/App.vue
+++ vue_cil/src/App.vue
@@ -41,4 +41,31 @@
    text-overflow:ellipsis;
 
 }
+/* 初始化样式 */
+#app-container{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width:100%;
+  height:100%;
+}
+*{
+  margin:0;
+  padding:0;
+}
+html,body{
+  width:100%;
+  height:100%;
+}
+ul{list-style:none}
+a{text-decoration: none;} 
+img{vertical-align:top;border:none}
+ 
+.clearf:after,
+.clearf:before{
+    content:"";
+    display:block;
+    clear:both;
+}
+
 </style>
